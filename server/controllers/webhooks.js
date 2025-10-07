9abe7ddbbdc15ca563a1b47f27e72c7ebc6aa7a5
PrasadPapppu <pvvprasad2105@gmail.com>
2025-10-07 16:42:28 +0530

    corrected user creation 2nd

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -7,7 +7,7 @@ import User from "../models/User.js";
 export const clerkWebhooks = async (req, res)=>{
     try{
         const whook =new Webhook(process.env.CLERK_WEBHOOK_SECRET)
-        await whook.verify(json.stringify(req.body),{
+        await whook.verify(JSON.stringify(req.body),{
             "svix-id": req.headers["svix-id"],
             'svix-timestamp':req.headers["svix-timestamp"],
             "svix-signature":req.headers["svix-signature"],
@@ -18,7 +18,7 @@ export const clerkWebhooks = async (req, res)=>{
             case 'user.created':{
                 const userData = {
                     _id:data.id,
-                    email:data.email_address[0].email_address,
+                    email:data.email_addresses[0].email_address,
                     name:data.first_name + " " + data.last_name,
                     imageUrl: data.image_url,
                 }
@@ -29,7 +29,7 @@ export const clerkWebhooks = async (req, res)=>{
             case 'user.updated':{
                 const userData = {
                     _id:data.id,
-                    email:data.email_addresses[0].email_address,
+                    email:data.email_address[0].email_address,
                     name:data.first_name + " " + data.last_name,
                     imageUrl: data.image_url,
             }
